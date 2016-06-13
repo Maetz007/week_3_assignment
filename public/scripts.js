6d3be78de2d5832e29cd38780ea7bbfd1f595fe5
Maetz007 <maetz007@yahoo.com>
2016-06-13 15:06:28 -0500

    hard mode touch ups

--- public/scripts.js
+++ public/scripts.js
@@ -1,22 +1,24 @@
 
 $(document).ready(function(){
 
+// creates a placeholder object to be passed to server
 var calcInput = {
   "x": "",
   "y": "",
   "type": ""
 }; // end calcInput object
 
-var numArray = [];
-var arraySums = "";
-var arrayCounter = 0;
+var numArray = []; // array to hold X or Y value
+var displayArray = ""; // array to display the combined buttons clicked
+var arraySums = ""; // placeholder variable for either X or Y variable
+var arrayCounter = 0; // sets counter to reference instances of amount of number button clicks
 
 // when button is clicked, displays clicked number on page
 $(".btnNum").css("background-color", "#00FF66").on("click", function(){
   var getNum = $(this).val();
-  numArray.push(getNum);
-  removeCommas(); // cleans up number array and creates a number variable
-  numOutput(arraySums); // outputs cleaned up array
+  numArray.push(getNum); // adds a single instance of a number to the number array
+  updateNumArrays(); // cleans up number array and creates a number variable
+  numOutput(displayArray); // outputs cleaned up array
 }); // end numbered button
 
 // determines the type of math to be used on the numbers
@@ -24,11 +26,10 @@ $(".calcFunc").css("background-color", "#0099FF").on("click", function(){
   calcInput.x = arraySums; // returns the X number to the object to be POSTed to server
   calcInput.type = $(this).val(); // returns the type of math to the object to be POSTed
   var showSymbol = $(this).attr("id"); // displays the type of math to be used
-  removeCommas();
-  numOutput(showSymbol);
-  numArray = [];        // the next couple of lines here reset all the variables
-  arraySums = "";       // and gets ready to take in inputs for the Y variable
-  arrayCounter = 0;
+  numArray.push(showSymbol); // adds the symbol of the calc function used to array for display
+  updateNumArrays();
+  numOutput(displayArray);
+  resetValues(); // resets all variables to the beginning so that we can capture a new, Y variable
 }); // end calcFunc button call
 
 // Passes the next number into the Y variable of the object and sends the object to the server
@@ -39,10 +40,11 @@ $("#equalBtn").css("background-color", "#FF7F42").on("click", function(){
 
 // Clears the all the fields and resets the display
 $("#reset").css("background-color", "#FF1122").on("click", function(){
-  numArray = [];
-  arraySums = "";
-  arrayCounter = 0;
+  displayArray = "";
+  resetValues();
   document.getElementById("numDisplay").innerHTML = "Enter a number...";
+  document.getElementById("displayCalc").innerHTML = "<br>Answer is...<br>";
+
 }); // end reset button
 
 // function call to the server to recieve the X, Y, and Type information then returns the data
@@ -51,8 +53,8 @@ function calculate(){
     type: "POST",
     data: calcInput, // keys the "data" field to the object
     url: "/calcPost", // location on the server where the POST will be sent
-    success: function(data){
-      numOutput(data); // displays returned number after server has done the "math"
+    success: function(dataIn){
+      displayCalculation(dataIn); // displays server calculation
     },
     error: function(){
       console.log("Oops, ajax broke!");
@@ -68,10 +70,25 @@ function numOutput(numToDisplay){
   document.getElementById("numDisplay").appendChild(newNumDisplay);
 } // end number display
 
+function displayCalculation(finalCalc){
+  var finalDisplay = document.createElement("div");
+  finalDisplay.textContent = finalCalc;
+  document.getElementById("displayCalc").innerHTML = "<br>Answer is...<br>";
+  document.getElementById("displayCalc").appendChild(finalDisplay);
+}
+
 // parses arrays into clean, readable numbers while also setting a variable to number to be POSTed later
-function removeCommas(){
-  arraySums = arraySums + numArray[arrayCounter];
+function updateNumArrays(){
+  arraySums = arraySums + numArray[arrayCounter]; // temp array that gets reset after calc chosen
+  displayArray = displayArray + numArray[arrayCounter];
   arrayCounter++;
 } // end clean up of array display to remove commas
+
+// resets all the values to clean up either the number display or X variable content
+function resetValues(){
+  numArray = [];
+  arraySums = "";
+  arrayCounter = 0;
+}
 
 }); // end document ready
